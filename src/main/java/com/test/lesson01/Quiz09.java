6be3cd0d19ca3b68bf31e8371d1ea46e1db38f53
301-05\User <2420205@naver.com>
2022-01-26 20:23:28 +0900

    01/26

--- src/main/java/com/test/lesson01/Quiz09.java
+++ src/main/java/com/test/lesson01/Quiz09.java
@@ -19,13 +19,10 @@ public class Quiz09 extends HttpServlet{
 		String introduce = request.getParameter("introduce");
 		
 		PrintWriter out = response.getWriter();
-		out.print("<html><head><title></title></head><body>");
-		out.print("<strong>" + name +"</strong>" + "님의 지원이 완료되었습니다.");
-		out.print("<br><strong><h3>" + "지원 내용" + "</h3><br>");
+		out.print("<html><head><title>입사 지원서</title></head><body>");
+		out.print("<strong>" + name +"</strong>님의 지원이 완료되었습니다.");
+		out.print("<br><strong><h3>" + "지원 내용" + "</h3></strong><br>");
 		out.print(introduce);
 		out.print("</body></html>");
-		
-
-		
 	}
 }
